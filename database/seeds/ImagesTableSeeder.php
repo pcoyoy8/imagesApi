476880da0976c56ad4b5519ce5b1b8e0131025ef
Pablo Coyoy <pcoyoy8@gmail.com>
2020-03-17 21:50:53 +0100

    Add routes, controller and seeder

---
+++ database/seeds/ImagesTableSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Image;
+
+class ImagesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = Faker\Factory::create();
+
+        for ($i = 0; $i < 10 ; $i++)
+        {
+            Image::create([
+                'picture_title' => $faker->uuid,
+                'picture_url' => $faker->url,
+                'picture_description' => $faker->macAddress
+            ]);
+        }
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('getAll', 'ImageController@index');
+Route::get('getById/{id}', 'ImageController@show');
